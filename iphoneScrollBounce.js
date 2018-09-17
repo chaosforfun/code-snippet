06e97f5b6eb3dc1f2bd2a803cc198577dbd84f4f
chaosforfun <chaos.forfun@gmail.com>
2018-09-17 11:11:45 +0800

    Update iphoneScrollBounce.js

--- iphoneScrollBounce.js
+++ iphoneScrollBounce.js
@@ -1,19 +1,19 @@
+let content = this.node
+    content.addEventListener('touchstart', function (event) {
+      let pageY = event.touches[0].pageY
+      this.allowUp = (this.scrollTop > 0);
+      this.allowDown = (this.scrollTop < (this.scrollHeight - this.clientHeight));
+      this.slideBeginY = pageY;
+    });
 
-var content = document.getElementsById('scrollDiv');
-content.addEventListener('touchstart', function (event) {
-    this.allowUp = (this.scrollTop > 0);
-    this.allowDown = (this.scrollTop < this.scrollHeight - this.clientHeight);
-    this.slideBeginY = event.pageY;
-});
-
-content.addEventListener('touchmove', function (event) {
-    var up = (event.pageY > this.slideBeginY);
-    var down = (event.pageY < this.slideBeginY);
-    this.slideBeginY = event.pageY;
-    if ((up && this.allowUp) || (down && this.allowDown)) {
+    content.addEventListener('touchmove', function (event) {
+      let pageY = event.touches[0].pageY
+      let up = (pageY > this.slideBeginY);
+      let down = (pageY < this.slideBeginY);
+      this.slideBeginY = pageY;
+      if ((up && this.allowUp) || (down && this.allowDown)) {
         event.stopPropagation();
-    }
-    else {
+      } else {
         event.preventDefault();
-    }
-});
+      }
+    });
