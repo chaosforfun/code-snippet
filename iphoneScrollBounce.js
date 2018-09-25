5283ce2422d63522c298d7b131f56990598e4dbd
chaosforfun <chaos.forfun@gmail.com>
2018-09-25 11:48:52 +0800

    Update iphoneScrollBounce.js

--- iphoneScrollBounce.js
+++ iphoneScrollBounce.js
@@ -1,27 +1,26 @@
-function scrollable(node, container) {
+function getScrollContainer(node, container) {
   let parent = node
   while (parent !== container) {
     let overflow = getComputedStyle(parent).overflowY
     if (overflow === 'scroll' || overflow === 'auto') {
-      return true
+      return parent
     }
     parent = parent.parentNode
   }
-  return false
+  return parent
 }
 
 let content = this.node
     if (!content) return
     content.addEventListener('touchstart', function (event) {
       let pageY = event.touches[0].pageY
-      this.allowUp = (this.scrollTop > 0);
-      this.allowDown = (this.scrollTop < (this.scrollHeight - this.clientHeight));
+      let container = getScrollContainer(event.touches[0].target, content)
+      this.allowUp = (container.scrollTop > 0);
+      this.allowDown = (container.scrollTop < (container.scrollHeight - container.clientHeight));
       this.slideBeginY = pageY;
-      this.skip = scrollable(event.touches[0].target, content)
     });
 
     content.addEventListener('touchmove', function (event) {
-      if (this.skip) return
       let pageY = event.touches[0].pageY
       let up = (pageY > this.slideBeginY);
       let down = (pageY <= this.slideBeginY);
