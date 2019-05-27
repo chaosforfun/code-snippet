92310ce6283c9f4f9494574cb0027799e443156d
chaosforfun <chaos.forfun@gmail.com>
2019-05-27 16:05:14 +0800

    添加开发者时长统计

--- wikiComputeTotal.js
+++ wikiComputeTotal.js
@@ -1,5 +1,10 @@
-[].slice.apply(document.querySelectorAll('.confluenceTable tr')).map(tr => {
-let h = tr.children[6].innerText
+coderMap = {}
+let total = [].slice.apply(document.querySelectorAll('.confluenceTable tr')).map(tr => {
+let h = tr.children[3].innerText
+let coder = tr.children[4].innerText
+coderMap[coder] = coderMap[coder] || 0
+coderMap[coder] += parseInt(h)
 h = parseInt(h, 10) || 0
 return h
 }).reduce((s, h) => s + h, 0)
+console.log(total, coderMap)
