1b650c0c34b15477fe44859044aaf223a86204ef
chaosforfun <chaos.forfun@gmail.com>
2019-06-10 19:26:16 +0800

    Update wikiComputeTotal.js

--- wikiComputeTotal.js
+++ wikiComputeTotal.js
@@ -1,7 +1,12 @@
+function lastIndexValue(arr, i) {
+  let l = arr.children.length - 1;
+  let index = l - i;
+  return arr.children[index].innerText;
+}
 coderMap = {}
 let total = [].slice.apply(document.querySelectorAll('.confluenceTable tr')).map(tr => {
-let h = tr.children[3].innerText
-let coder = tr.children[4].innerText
+let h = lastIndexValue(tr, 1)
+let coder = lastIndexValue(tr, 0)
 coderMap[coder] = coderMap[coder] || 0
 coderMap[coder] += parseInt(h)
 h = parseInt(h, 10) || 0
