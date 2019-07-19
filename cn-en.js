2a063fde40c57103ec09a659688060e33aeb2ef2
chaosforfun <chaos.forfun@gmail.com>
2019-07-19 14:53:21 +0800

    Update cn-en.js
    去除换行

--- cn-en.js
+++ cn-en.js
@@ -3,7 +3,7 @@ function getEn(table, baseIndex) {
     Array.from(table.querySelectorAll('tr'))
         .forEach((tr) => {
             let tds = Array.from(tr.children)
-            str += `"${tds[baseIndex].innerText}":"${tds[baseIndex + 1].innerText}",\n`;
+            str += `"${tds[baseIndex].innerText.replace('\n', '')}":"${tds[baseIndex + 1].innerText.replace('\n', '')}",\n`;
         });
     console.log(str);
     return str;
